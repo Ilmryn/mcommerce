53c00448de1f7b8525b98cbfadcab16332789626
QuiniouS <QuiniouS@girtab-sab3.vannes.sab2i.local>
2020-05-10 11:53:37 +0200

    Zipkin

---
+++ microservice-produits/src/main/java/com/mproduits/configurations/SleuthConfig.java
@@ -0,0 +1,16 @@
+package com.mproduits.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import brave.sampler.Sampler;
+
+@Configuration
+public class SleuthConfig {
+	
+	@Bean
+	public Sampler defaultSapmpler() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
+
+}
